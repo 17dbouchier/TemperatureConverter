576c0af67652e7a80e57dc9bcb83edeba7c1f340
Dillon Bouchier <dillonbouchier@Johannes-Ewan-Mackays-Mackaybook-Pro.local>
2020-03-26 14:37:40 +1300

    Added fahrenheit functionality in Javascript.

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const celsiusInput = document.querySelector('#celsius > input');
 const fahrenheitInput = document.querySelector('#fahrenheit > input');
 const kelvinInput = document.querySelector('#kelvin > input');
 
-celsiusInput.addEventListener('input', function() {
+function celsiusToFahrenheitAndKelvin() {
   const cTemp = parseFloat(celsiusInput.value);
   const fTemp = (cTemp * (9/5)) + 32;
   const kTemp = cTemp + 273.15;
   fahrenheitInput.value = fTemp;
   kelvinInput.value = kTemp;
-});
+}
+
+function fahrenheitToCelsiusAndKelvin() {
+  const fTemp = parseFloat(fahrenheitInput.value);
+  const cTemp = (fTemp - 32) * (5/9);
+  const kTemp = (fTemp + 459.67) * (5/9);
+  celsiusInput.value = cTemp;
+  kelvinInput.value = kTemp;
+}
+
+celsiusInput.addEventListener('input', celsiusToFahrenheitAndKelvin);
