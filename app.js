7fba4ec8d9e0eb8e2bb0f90a096263844d0d44fb
Dillon Bouchier <dillonbouchier@Johannes-Ewan-Mackays-Mackaybook-Pro.local>
2020-03-26 14:59:56 +1300

    Fixed bugs causing numbers not to display and fixed a rounding bug.

--- app.js
+++ app.js
@@ -10,26 +10,26 @@ function celsiusToFahrenheitAndKelvin() {
   const cTemp = parseFloat(celsiusInput.value);
   const fTemp = (cTemp * (9/5)) + 32;
   const kTemp = cTemp + 273.15;
-  fahrenheitInput.value = fTemp;
-  kelvinInput.value = kTemp;
+  fahrenheitInput.value = roundNum(fTemp);
+  kelvinInput.value = roundNum(kTemp);
 }
 
 function fahrenheitToCelsiusAndKelvin() {
   const fTemp = parseFloat(fahrenheitInput.value);
   const cTemp = (fTemp - 32) * (5/9);
   const kTemp = (fTemp + 459.67) * (5/9);
-  celsiusInput.value = cTemp;
-  kelvinInput.value = kTemp;
+  celsiusInput.value = roundNum(cTemp);
+  kelvinInput.value = roundNum(kTemp);
 }
 
 function kelvinToCelsiusAndFahrenheit() {
   const kTemp = parseFloat(kelvinInput.value);
   const cTemp = kTemp - 273.15;
   const fTemp = 9/5 * (kTemp - 273) + 32;
-  celsiusInput.value = cTemp;
-  fahrenheitInput.value = fTemp;
+  celsiusInput.value = roundNum(cTemp);
+  fahrenheitInput.value = roundNum(fTemp);
 }
 
 celsiusInput.addEventListener('input', celsiusToFahrenheitAndKelvin);
-celsiusInput.addEventListener('input', fahrenheitToCelsiusAndKelvin);
-celsiusInput.addEventListener('input', kelvinToCelsiusAndFahrenheit);
+fahrenheitInput.addEventListener('input', fahrenheitToCelsiusAndKelvin);
+kelvinInput.addEventListener('input', kelvinToCelsiusAndFahrenheit);
